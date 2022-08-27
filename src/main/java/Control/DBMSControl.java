7e335fc79eee8242d6bf70d0ab1301e01f4bff5e
Bruno-Feraz <79698951+Bruno-Feraz@users.noreply.github.com>
2022-08-27 17:40:29 +0200

    update2
    idem

--- src/main/java/Control/DBMSControl.java
+++ src/main/java/Control/DBMSControl.java
@@ -28,29 +28,6 @@ public class DBMSControl {
             }
         return conn;
     }
-     //   Statement st = (Statement)conn.createStatement();
-       // ResultSet rs = st.executeQuery("select * from "+tabella);
+    
         
-}/*
-public Connection getConnection(){
-Connection conn;
-try{
-conn = DriverManager.getConnection("");
-return conn;
-} catch (Exception e){
-return null;
 }
-public void executeQuery(String query, String message){
-Connection conn = getConnection();
-Statement st;
-try{
-st = conn.createStatement();
-if((st.executeUpdate(query)) == 1){
-JOptionPane.showMessageDialog("FATTO");
-}else{
-JOptionPane.showMessageDialog("FATTO");
-}
-}catch(Exception e){
-System.out.println(e);
-}
-}*/
