25c1abe9e73d7aae6b054e9733e501e10a7d4e1f
Manfredi Crapa <53235091+KengiY@users.noreply.github.com>
2022-08-27 16:46:56 +0200

    Tolte le Control Inutili

--- src/main/java/com/mycompany/farmacysoftware/G_Prenotazione/AggiungiFarmaco.java
+++ src/main/java/com/mycompany/farmacysoftware/G_Prenotazione/AggiungiFarmaco.java
@@ -181,7 +181,7 @@ public class AggiungiFarmaco extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(bottoneIndietro)
-                .addContainerGap())
+                .addGap(16, 16, 16))
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
